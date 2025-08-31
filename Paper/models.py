71b36663b977dfed76f3b353bbf6931ff0c2c404
Bisma <bismashehzadi842@gmail.com>
2025-08-31 02:32:15 -0700

    i have created Admin db

---
+++ Paper/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomAdmin(AbstractUser):
+    role = models.CharField(
+        max_length= 20,
+        choices=[('Admin','admin'),('Teachers','teachers')]
+        default= 'Teachers'
+        
+    )
