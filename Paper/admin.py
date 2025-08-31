13dd1b6b45e98a0b18dedd85ad3c2be4327ef84c
Bisma <bismashehzadi842@gmail.com>
2025-08-31 03:03:43 -0700

    i've registered custom model in admin

--- Paper/admin.py
+++ Paper/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import CustomAdmin
 # Register your models here.
+
+
+class MyAdmin(admin.ModelAdmin):
+    model = CustomAdmin
+    list_display = ['username','email','role','is_staff','is_superuser']
+admin.site.register(MyAdmin,CustomAdmin)
+    
