66df06beb83459c11085b22728e525076f5637d1
Bisma <bismashehzadi842@gmail.com>
2025-09-01 13:25:36 -0700

    model issue solved

--- Paper/models.py
+++ Paper/models.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import AbstractUser
 class CustomAdmin(AbstractUser):
     role = models.CharField(
         max_length= 20,
-        choices=[('Admin','admin'),('Teachers','teachers')]
+        choices=[('Admin','admin'),('Teachers','teachers')],
         default= 'Teachers'
         
     )
+
+# Create your models here.
