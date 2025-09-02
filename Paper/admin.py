c278daf63a947263cd2827387992bf4cdaf057b1
BISMASHEHZADI <157670590+BISMASHEHZADI@users.noreply.github.com>
2025-09-01 22:15:14 -0700

    Update admin.py

--- Paper/admin.py
+++ Paper/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CustomAdmin
+# Register your models here.
+
+
+class MyAdmin(admin.ModelAdmin):
+    model = CustomAdmin
+    list_display = ['username','email','role','is_staff','is_superuser']
+admin.site.register(CustomAdmin,MyAdmin)
+    
