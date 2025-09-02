65c5aa882f7201d79c9022a9ee3f7df80610c162
BISMASHEHZADI <157670590+BISMASHEHZADI@users.noreply.github.com>
2025-09-01 23:01:59 -0700

    again Update settings.py

--- PaperMaker/settings.py
+++ PaperMaker/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'PaperMaker.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        
+        'NAME':'school_db',
+        'USER': 'postgres',
+        'PASSWORD': '000_bshehzadi',
+        'HOST': 'localhost',
+        'PORT':'5432',
+            
+            
+        
+
+                }
 }
 
 
