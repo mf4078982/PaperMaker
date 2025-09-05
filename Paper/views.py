bf5592eb34c28d18f4ebb49a50235f05cc205d33
mf4078982 <mf4078982@gmail.com>
2025-09-05 09:09:59 -0700

    Refactored pages to use base template and added Users/About pages

--- Paper/views.py
+++ Paper/views.py
@@ -10,4 +10,13 @@ def help(request):
     return render(request, "help.html") 
 
 def send(request):
+<<<<<<< HEAD
     return render(request, "sendfeed.html")
+=======
+    return render(request, "sendfeed.html")
+def about(request):
+    return render(request, 'about.html')
+
+def userList(request):
+    return render(request, 'user_list.html')
+>>>>>>> ffc18c8 (Refactored pages to use base template and added Users/About pages)
