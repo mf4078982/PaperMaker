8bb14b6ea80abc949764a7d00fe0ef0611c06eb0
mf4078982 <mf4078982@gmail.com>
2025-09-08 00:59:34 -0700

    Save submodule changes

--- Paper/urls.py
+++ Paper/urls.py
@@ -6,12 +6,9 @@ urlpatterns = [
     path("",views.home,name="home"),
     path("help/",views.help,name="help"),
     path("send/",views.send,name="send"),
-<<<<<<< HEAD
-=======
     path("about/",views.about,name="about"),
     path("user/",views.userList,name="userList"),
     
     
->>>>>>> ffc18c8 (Refactored pages to use base template and added Users/About pages)
 ]
 
